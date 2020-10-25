2f9eefb275a2d19a2ac4e508ec47d0b65c0430f6
Shawal Kabir Chy <shawalkabirchy2020@gmail.com>
2020-10-25 20:24:24 +0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
+import Imessage from "./Imessage";
+import Login from "./Login";
+
+function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
+  return <div className="App">{user ? <Imessage /> : <Login />}</div>;
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
